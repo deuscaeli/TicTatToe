cf8f49133372c42fd7529a7fdb0a35d4fd08d5ad
deuscaeli <zyu18@illinois.edu>
2014-11-04 11:24:54 -0600

    e

--- app/src/main/java/com/cs126/jamesyu/myapplication/MainActivity.java
+++ app/src/main/java/com/cs126/jamesyu/myapplication/MainActivity.java
@@ -4,6 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +22,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView victoryMessage = (TextView) findViewById(R.id.victoryMessage);
+        final TextView currentPlayer = (TextView) findViewById(R.id.currentPlayer);
+        final Button topLeft = (Button) findViewById(R.id.topLeft);
+        Button top = (Button) findViewById(R.id.top);
+        Button topRight = (Button) findViewById(R.id.topRight);
+        Button left = (Button) findViewById(R.id.left);
+        Button mid = (Button) findViewById(R.id.mid);
+        Button right = (Button) findViewById(R.id.right);
+        Button botLeft = (Button) findViewById(R.id.botLeft);
+        Button bot = (Button) findViewById(R.id.bot);
+        Button botRight = (Button) findViewById(R.id.botRight);
+
+        topLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                topLeft.setText(currentPlayer.getText());
+            }
+        });
     }
 
 
@@ -33,4 +62,8 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public boolean Text[];
 }
